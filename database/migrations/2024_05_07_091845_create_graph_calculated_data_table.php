23adc90938f0071ba0087bb210c26ec0f8e8bf3f
Reingalrt <adorufus@gmail.com>
2024-05-08 16:46:52 +0700

    commit

--- database/migrations/2024_05_07_091845_create_graph_calculated_data_table.php
+++ database/migrations/2024_05_07_091845_create_graph_calculated_data_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
         Schema::create('graph_calculated_data', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('reach');
-            $table->string('total_interactions');
-            $table->string('comments');
-            $table->string('ig_reels_avg_watch_time');
-            $table->string('ig_reels_video_view_total_time');
-            $table->string('likes');
-            $table->string('plays');
-            $table->string('saved');
-            $table->string('shares');
+            $table->bigInteger('reach');
+            $table->bigInteger('total_interactions');
+            $table->bigInteger('comments');
+            $table->bigInteger('ig_reels_avg_watch_time');
+            $table->bigInteger('ig_reels_video_view_total_time');
+            $table->bigInteger('likes');
+            $table->bigInteger('plays');
+            $table->bigInteger('saved');
+            $table->bigInteger('shares');
         });
     }
 
