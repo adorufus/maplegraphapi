2ab0a4d42cbf520e1af5f9311117e47de5753965
reingalrt <adorufus@gmail.com>
2024-05-10 13:26:11 +0700

    commit

--- database/migrations/2024_05_07_091845_create_graph_calculated_data_table.php
+++ database/migrations/2024_05_07_091845_create_graph_calculated_data_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
         Schema::create('graph_calculated_data', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->bigInteger('reach')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('total_interactions')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('comments')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('ig_reels_avg_watch_time')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('ig_reels_video_view_total_time')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('likes')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('plays')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('saved')->collation('utf8mb4_unicode_ci')->change();
-            $table->bigInteger('shares')->collation('utf8mb4_unicode_ci')->change();
+            $table->bigInteger('reach')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('total_interactions')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('comments')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('ig_reels_avg_watch_time')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('ig_reels_video_view_total_time')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('likes')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('plays')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('saved')->collation('utf8mb4_unicode_ci');
+            $table->bigInteger('shares')->collation('utf8mb4_unicode_ci');
         });
     }
 
