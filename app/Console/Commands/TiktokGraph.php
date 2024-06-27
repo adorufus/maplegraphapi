45bc37cae182ae7234fe3a2612557d0a10f453ac
reingalrt <adorufus@gmail.com>
2024-06-27 14:20:01 +0700

    commit

---
+++ app/Console/Commands/TiktokGraph.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TiktokToken;
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+
+class TiktokGraph extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tiktok:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get metrics from tiktok api v2';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $tiktokTokenModel = new TiktokToken;
+        $httpClient = new Client();
+        $firstIndexTokenModel = $tiktokTokenModel->first->toArray();
+
+        $url = 'https://open-api.tiktok.com/video/list/';
+
+        $httpClient->postAsync($url, [
+            'headers' => [
+                'Content-Type' => 'application/json'
+            ],
+            'form_params' => [
+                'access_token' => $firstIndexTokenModel['access_token '],
+                'fields' => ['id', 'title', 'like_count', 'comment_count', 'share_count', 'view_count']
+            ]
+        ])->then(function ($response) {
+            echo json_decode($response->getBody());
+        });
+
+    }
+}
